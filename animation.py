b1318768a4ccea9dcd69d92b355478038abbc93f
bb515 <bb515@cam.ac.uk>
2023-12-20 09:59:38 +0000

    added code that produces animated GIF of sampling a bimodal with image of colourmapped marginals

--- animation.py
+++ animation.py
@@ -1,4 +1,4 @@
-"""Animated GIF of posterior sampling a bimodal."""
+"""Animated GIF of sampling a bimodal with marginals colormapped image."""
 from absl import app, flags
 from ml_collections.config_flags import config_flags
 import logging
@@ -11,7 +11,6 @@ from diffusionjax.run_lib import get_ddim_chain
 from diffusionjax.utils import get_sampler
 import diffusionjax.sde as sde_lib
 from tmpd.samplers import get_cs_sampler
-from tmpd.plot import plot_animation
 import torch
 from torch import eye, randn_like, vstack, manual_seed
 import numpyro.distributions as dist
@@ -25,10 +24,11 @@ import time
 from cycler import cycler
 from matplotlib import cm
 from matplotlib.gridspec import GridSpec
+import matplotlib.animation as animation
 
 new_prop_cycle = cycler('color', ['r', 'g', 'b'])
 
-
+BG_COLOUR = (239/255, 239/255, 239/255)
 BG_ALPHA = 1.0
 MG_ALPHA = 1.0
 FG_ALPHA = 0.3
@@ -37,7 +37,9 @@ FG_ALPHA = 0.3
 dpi_val = 1200
 cmap = 'magma'
 
+
 ddim_methods = ['PiGDMVP', 'PiGDMVE', 'DDIMVE', 'DDIMVP', 'KGDMVP', 'KGDMVE']
+
 
 FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file(
@@ -45,6 +47,115 @@ config_flags.DEFINE_config_file(
 flags.DEFINE_string("workdir", "./workdir", "Work directory.")
 flags.mark_flags_as_required(["workdir", "config"])
 logger = logging.getLogger(__name__)
+
+
+def plot_animation(fig, ax, animate, frames, fname, fps=20, bitrate=800, dpi=300):
+  ani = animation.FuncAnimation(
+    fig, animate, frames=frames, interval=1, fargs=(ax,))
+  # Set up formatting for the movie files
+  Writer = animation.PillowWriter
+  writer = Writer(fps=fps, metadata=dict(artist='Me'), bitrate=bitrate)
+  ani.save('{}.gif'.format(fname), writer=writer, dpi=dpi)
+
+
+def plot_marginals1(x_lims, ou_dist, sde):
+    res = 100
+    hires = 2000
+    t_lims = (1., 0.)
+    x = np.linspace(x_lims[0], x_lims[1], res)
+    x_hires = np.linspace(x_lims[0], x_lims[1], hires)
+    # t_hires = np.array([0.80, 0.65, 0.45, 0.20])
+    # t_hires = np.array([0.90, 0.55, 0.39, 0.10])
+    # t_hires = np.array([0.95, 0.47, 0.35, 0.05])
+    # t_hires = np.array([0.97, 0.46, 0.34, 0.03])
+    t_hires = np.array([0.98, 0.45, 0.34, 0.01])
+    # t_hires = np.array([1., 0.45, 0.33, 0.])
+    t = np.linspace(t_lims[0], t_lims[1], res)
+    xx, tt = np.meshgrid(x, t)
+    xx_hires, tt_hires = np.meshgrid(x_hires, t_hires)
+    xx = xx.flatten().reshape(-1, 1)
+    tt = tt.flatten().reshape(-1, 1)
+    xx_hires = xx_hires.flatten().reshape(-1, 1)
+    tt_hires = tt_hires.flatten().reshape(-1, 1)
+    fn = vmap(lambda x, t: ou_dist(sde.mean_coeff(t)).log_prob(x))
+    vals = fn(xx, tt)
+    hires_vals = fn(xx_hires, tt_hires)
+    vals = vals.reshape((res, res))
+    hires_vals = hires_vals.reshape((np.size(t_hires), hires))
+    figure_sizes = {'fullwidth': 10}
+
+    # Grid spec
+    fig = plt.figure(figsize=(figure_sizes['fullwidth'], 0.4*figure_sizes['fullwidth']))
+    fig.set_facecolor(BG_COLOUR)
+    gs = GridSpec(1, 4, width_ratios=[1, 1, 1, 1])
+    gs.update(left=0.075,
+          right=0.92,
+          top=0.8,
+          bottom=0.2,
+          wspace=0.2,hspace=0.08)
+    axs = np.array([fig.add_subplot(gs[0, i]) for i in range(4)])
+
+    # ax = fig.add_subplot(gs[1, :])
+    vmax = np.max(vals)
+    vmin = np.min(vals)
+
+    s = 8
+
+    axs[0].scatter(jnp.exp(hires_vals[-1]), x_hires, c=hires_vals[-1], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax, s=s)
+    axs[1].scatter(jnp.exp(hires_vals[2]), x_hires, c=hires_vals[2], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax, s=s)
+    axs[2].scatter(jnp.exp(hires_vals[1]), x_hires, c=hires_vals[1], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax, s=s)
+    axs[3].scatter(jnp.exp(hires_vals[0]), x_hires, c=hires_vals[0], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax, s=s)
+
+    fontsize = 20
+    # # axs[0].axis('off')
+    # # axs[0].set_frame_on(False)
+    # axs[0].set_xlabel("$p_{n=N}(x_{N})$", fontsize=fontsize)
+    # # axs[0].get_xaxis().set_visible(False)
+    # axs[0].get_yaxis().set_visible(False)
+
+    # # axs[1].axis('off')
+    # # axs[1].set_frame_on(False)
+    # axs[1].set_xlabel("$p_{n=2}(x_{2})$", fontsize=fontsize)
+    # # axs[1].get_xaxis().set_visible(False)
+    # axs[1].get_yaxis().set_visible(False)
+
+    # # axs[2].axis('off')
+    # # axs[2].set_frame_on(False)
+    # axs[2].set_xlabel("$p_{n=1}(x_{1})$", fontsize=fontsize)
+    # # axs[2].get_xaxis().set_visible(False)
+    # axs[2].get_yaxis().set_visible(False)
+
+    # # axs[3].axis('off')
+    # # axs[3].set_frame_on(False)
+    # axs[3].set_xlabel("$p_{n=0}(x_{0})$", fontsize=fontsize)
+    # # axs[3].get_xaxis().set_visible(False)
+    # axs[3].get_yaxis().set_visible(False)
+
+
+    axs[0].set_xlabel("$p_{n=0}(x_{0})$", fontsize=fontsize)
+    # axs[0].get_xaxis().set_visible(False)
+    axs[0].get_yaxis().set_visible(False)
+
+    # axs[1].axis('off')
+    # axs[1].set_frame_on(False)
+    axs[1].set_xlabel("$p_{n=1}(x_{1})$", fontsize=fontsize)
+    # axs[1].get_xaxis().set_visible(False)
+    axs[1].get_yaxis().set_visible(False)
+
+    # axs[2].axis('off')
+    # axs[2].set_frame_on(False)
+    axs[2].set_xlabel("$p_{n=2}(x_{2})$", fontsize=fontsize)
+    # axs[2].get_xaxis().set_visible(False)
+    axs[2].get_yaxis().set_visible(False)
+
+    # axs[3].axis('off')
+    # axs[3].set_frame_on(False)
+    axs[3].set_xlabel("$p_{n=N}(x_{N})$", fontsize=fontsize)
+    # axs[3].get_xaxis().set_visible(False)
+    axs[3].get_yaxis().set_visible(False)
+
+
+    return fig, axs
 
 
 def plot_marginals(x_lims, ou_dist, sde):
@@ -71,6 +182,8 @@ def plot_marginals(x_lims, ou_dist, sde):
     figure_sizes = {'fullwidth': 10}
     # Grid spec
     fig = plt.figure(figsize=(figure_sizes['fullwidth'], 0.4*figure_sizes['fullwidth']))
+    fig.set_facecolor(BG_COLOUR)
+
     gs = GridSpec(2, 3, width_ratios=[1, 3.3, 1], height_ratios=[1, 1])
     gs.update(left=0.075,
           right=0.92,
@@ -83,9 +196,10 @@ def plot_marginals(x_lims, ou_dist, sde):
     vmax = np.max(vals)
     vmin = np.min(vals)
 
-    axs[0].scatter(jnp.exp(hires_vals[0]), x_hires, c=hires_vals[0], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax)
+    s = 8
+    axs[0].scatter(jnp.exp(hires_vals[0]), x_hires, c=hires_vals[0], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax, s=s)
     axs[1].imshow(vals.T, extent=(t_lims + x_lims), interpolation='bicubic', cmap='magma')
-    axs[2].scatter(jnp.exp(hires_vals[-1]), x_hires, c=hires_vals[-1], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax)
+    axs[2].scatter(jnp.exp(hires_vals[-1]), x_hires, c=hires_vals[-1], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax, s=s)
 
     # axs[0].axis('off')
     # axs[0].set_frame_on(False)
@@ -115,6 +229,7 @@ def plot_marginals(x_lims, ou_dist, sde):
     axs[1].set_aspect(0.33/(x_lims[1] - x_lims[0]))
     axs[2].set_position([bbox1.x0 + width * bbox0.width, bbox2.y0 + bbox1.height * (1 - im_ratio) / 2, bbox2.width, bbox1.height * (im_ratio)])
 
+    bbox0 = axs[0].get_position()
     bbox1 = axs[1].get_position()
 
     # axs[3].axis('off')
@@ -124,7 +239,90 @@ def plot_marginals(x_lims, ou_dist, sde):
     axs[3].set_position([bbox1.x0, bbox1.y0 * (1. - 9./10), bbox1.height, bbox1.height])
 
     # fig.tight_layout()
-    return fig, axs, bbox1
+    return fig, axs, (bbox0, bbox1)
+
+
+def plot_posterior_marginals(x_lims, ou_dist, sde):
+    res = 100
+    hires = 2000
+    t_lims = (1., 0.)
+    x = np.linspace(x_lims[0], x_lims[1], res)
+    x_hires = np.linspace(x_lims[0], x_lims[1], hires)
+    t_hires = np.array([1., 0.])
+    t = np.linspace(t_lims[0], t_lims[1], res)
+    xx, tt = np.meshgrid(x, t)
+    xx_hires, tt_hires = np.meshgrid(x_hires, t_hires)
+    xx = xx.flatten().reshape(-1, 1)
+    tt = tt.flatten().reshape(-1, 1)
+    xx_hires = xx_hires.flatten().reshape(-1, 1)
+    tt_hires = tt_hires.flatten().reshape(-1, 1)
+    # fn = vmap(lambda x, t: jnp.exp(ou_dist(sde.mean_coeff(t)).log_prob(x)))
+    fn = vmap(lambda x, t: ou_dist(sde.mean_coeff(t)).log_prob(x))
+    vals = fn(xx, tt)
+    hires_vals = fn(xx_hires, tt_hires)
+    vals = vals.reshape((res, res))
+    hires_vals = hires_vals.reshape((2, hires))
+
+    figure_sizes = {'fullwidth': 10}
+    # Grid spec
+    fig = plt.figure(figsize=(figure_sizes['fullwidth'], 0.4*figure_sizes['fullwidth']))
+    fig.set_facecolor(BG_COLOUR)
+    gs = GridSpec(2, 3, width_ratios=[1, 3.3, 1], height_ratios=[1, 1])
+    gs.update(left=0.075,
+          right=0.92,
+          top=0.8,
+          bottom=0.2,
+          wspace=0.2,hspace=0.08)
+    axs = np.array([fig.add_subplot(gs[0, i]) for i in range(3)] + [fig.add_subplot(gs[1, :])])
+
+    # ax = fig.add_subplot(gs[1, :])
+    vmax = np.max(vals)
+    vmin = np.min(vals)
+
+    s = 8
+    axs[0].scatter(jnp.exp(hires_vals[0]), x_hires, c=hires_vals[0], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax, s=s)
+    axs[1].imshow(vals.T, extent=(t_lims + x_lims), interpolation='bicubic', cmap='magma')
+    axs[2].scatter(jnp.exp(hires_vals[-1]), x_hires, c=hires_vals[-1], edgecolor='None', cmap='magma', vmin=vmin, vmax=vmax, s=s)
+
+    # axs[0].axis('off')
+    # axs[0].set_frame_on(False)
+    axs[0].set_xlabel("$p_{t=1}(x_{t=1})$")
+    # axs[0].get_xaxis().set_visible(False)
+    axs[0].get_yaxis().set_visible(False)
+
+    axs[1].axis('off')
+    axs[1].set_frame_on(False)
+    axs[1].get_xaxis().set_visible(False)
+    axs[1].get_yaxis().set_visible(False)
+
+    # axs[2].axis('off')
+    # axs[2].set_frame_on(False)
+    axs[2].set_xlabel("$p_{t=0}(x_{t=0})$")
+    # axs[2].get_xaxis().set_visible(False)
+    axs[2].get_yaxis().set_visible(False)
+
+    bbox0 = axs[0].get_position()
+    bbox1 = axs[1].get_position()
+    bbox2 = axs[2].get_position()
+
+    # im_ratio = 0.67
+    im_ratio = 1.0
+    width = 1.29
+    axs[0].set_position([bbox1.x0 - 1.74 * width * bbox0.width, bbox0.y0 + bbox1.height * (1 - im_ratio) / 2, bbox0.width, bbox1.height * (im_ratio)])
+    axs[1].set_aspect(0.33/(x_lims[1] - x_lims[0]))
+    axs[2].set_position([bbox1.x0 + width * bbox0.width, bbox2.y0 + bbox1.height * (1 - im_ratio) / 2, bbox2.width, bbox1.height * (im_ratio)])
+
+    bbox0 = axs[0].get_position()
+    bbox1 = axs[1].get_position()
+
+    # axs[3].axis('off')
+    # axs[3].set_frame_on(False)
+    axs[3].get_xaxis().set_visible(False)
+    axs[3].get_yaxis().set_visible(False)
+    axs[3].set_position([bbox1.x0, bbox1.y0 * (1. - 9./10), bbox1.height, bbox1.height])
+
+    # fig.tight_layout()
+    return fig, axs, (bbox0, bbox1)
 
 
 def ot_sliced_wasserstein(seed, dist_1, dist_2, n_slices=100):
@@ -394,28 +592,46 @@ def main(argv):
                         ts = np.linspace(0., 1., config.solver.num_outer_steps + 1)[1:]
                         samples = samples.reshape(
                             config.solver.num_outer_steps, config.eval.batch_size, config.data.image_size)
+
                         frames = 100
                         x_lims = (-8., 8.)
-                        fig, axs, bbox1 = plot_marginals(x_lims, ou_mixt_jax_fun, sde)
-                        fig.savefig("test.png")
+                        fig, axs = plot_marginals1(x_lims, ou_mixt_jax_fun, sde)
+                        fig.savefig("test0.png")
+                        assert 0
+
+                        fig, axs, (bbox0, bbox1) = plot_marginals(x_lims, ou_mixt_jax_fun, sde)
+                        fig.savefig("test1.png")
+
                         axs[1].set_prop_cycle(new_prop_cycle)
                         axs[1].plot(ts[990].reshape(-1, 1), samples[990, 0, 0].reshape(-1, 1).T)
+                        npz = np.load('./assets/0.05_FFHQ_KPSMLDplus_1.npz')
+                        im_samples = npz['samples']
+                        (num_outer_steps, _, image_size, _, num_channels) = im_samples.shape
+                        bbox3_y0 = bbox1.y0 * (1. - 9./10)
                         def animate(i, axs):
                             for art in list(axs[1].lines):
                                 art.remove()
                             idx = config.solver.num_outer_steps - int((i + 1) * config.solver.num_outer_steps / frames)
+                            im_idx = num_outer_steps - int((i+1) * num_outer_steps / frames)
                             idxs = np.linspace(idx, int(config.solver.num_outer_steps * ( 1. - 1. / frames)), i + 1, dtype=int)
                             axs[1].plot(ts[idxs], samples[idxs, :, 0], linewidth="0.5")
-                            # axs[1].annotate("",
-                            #             xy=(0, 0), xycoords=axs[1].transData,
-                            #             xytext=(0, 0.1), textcoords=axs[1].transData,
-                            #             arrowprops=dict(arrowstyle="<->"))
-                            axs[3].set_position([bbox1.x0  + bbox1.width * (i / frames), bbox1.y0 * (1. - 9./10), bbox1.height, bbox1.height])
+                            bbox3_x0 = bbox0.x0 + bbox1.width * (i / frames)
+                            axs[3].set_position([bbox3_x0, bbox3_y0, bbox1.height, bbox1.height])
+                            im = im_samples[im_idx]
+                            im = im.reshape((image_size, image_size, num_channels))
+                            axs[3].imshow(im, interpolation=None)
+                            print(samples[idxs[0], 0, 0])
+                            sample_x0 = 256 * (0.5 + 3. / frames)
+                            axs[1].annotate("",
+                                        xy=(sample_x0, -150), xycoords=axs[3].transData,
+                                        xytext=(sample_x0, 0), textcoords=axs[3].transData,
+                                        arrowprops=dict(arrowstyle="<->"))
+                            fig.savefig("{}-{}.png".format('kpsmldplus', i))
 
                         plot_animation(fig, axs,
                                     animate, frames,
-                                    fname="{}_{}_{}".format(config.sampling.noise_std, config.sampling.cs_method.lower(), i),
-                                    bitrate=1000,
+                                    fname="{}_{}".format(0.05, 'kpsmldplus'),
+                                    bitrate=600,
                                     dpi=300)
 
                     else:
